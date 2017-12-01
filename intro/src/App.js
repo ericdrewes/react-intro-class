e7873bdb1ec09170b4df700d3dd9e9433e2a124d
ericdrewes <eric.d.drewes@gmail.com>
2017-12-01 14:23:31 -0600

    steps 5/9

---
+++ intro/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from './Calculator/Calculator';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
